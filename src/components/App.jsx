4e87bba34ac9911f42497a74484f4001300289ec
nataliakostyrko <155002144+nataliakostyrko@users.noreply.github.com>
2024-12-28 17:09:19 +0100

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,52 +1,60 @@
 import { useEffect, useState } from "react";
-import ContactForm from "./ContactForm/ContactForm";
-import SearchBox from "./SearchBox/SearchBox";
-import ContactList from "./ContactList/ContactList";
-import contacts from "..//assets/contact.json";
-import "./App.module.css";
-
+import "./App.css";
+import SearchBar from "./SearchBar/SearchBar";
+import fetchImages from "../services/api";
 
 
 function App() {
-    const localeContacts = () => {
-    const savedContacts = window.localStorage.getItem("Contacts");
-    return savedContacts ? JSON.parse(savedContacts) : contacts;
-    };
-    const [contact, setContact] = useState(localeContacts);
-    const [filter, setFilter] = useState("");
-
-
-useEffect(() => {
-    window.localStorage.setItem("Contacts", JSON.stringify(contact));
-  }, [contact]);
-   
-
-    const addContact = (newContact) => {
-        setContact((prevContact) => {
-            return [...prevContact, newContact];
-        });
-    };
-
-
-    const deleteContact = (contactId) => {
-        setContact((prevContact) => {
-            return prevContact.filter((contact) => contact.id !== contactId);
-        });
-    };
-    const searchContact = contact.filter((cont) =>
-        cont.name.toLowerCase().includes(filter.toLowerCase())
-    );
+    const [images, setImages] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
     
-    
-    
+
+
+    useEffect(() => {
+        if (!query) return;
+
+        const getArticles = async () => {
+            try {
+                setLoading(true);
+                setError(false);
+                
+                const response = await fetchImages(query, page);
+            
+                
+            } catch (error) {
+                setError(true);
+            } finally {
+                setLoading(false);
+            }
+        };
+        getArticles();
+        
+    }, [query, page]);
+    const handleSubmit = (query) => {
+    setQuery(query);
+    setImages([]);
+    setPage(1);
+  };
+
+
+
+
+
+
+
+
+
+
     return (
-    <div className ="container">
-  <h1>Phonebook</h1>
-            <ContactForm addContact={addContact} />
-            <SearchBox value={filter} onFilter={setFilter} />
-            <ContactList deleteContact={deleteContact} contact={searchContact} />
-    </div>
-);
-}
+        <div>
+            <h1>Gallery</h1>
+            <SearchBar onSubmit={handleSubmit} />
+            <ImageGallery/>
+            
+</div>
+    );
+};
+
 
 export default App;
